0473996b134cc9cdd35bd5a6b56fc05cfb91eb48
arnavsri454 <arnavsri454@gmail.com>
2025-03-07 15:40:43 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,8 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
+export const registerUser = async (username, email, password) => {
+  return api.post('/api/auth/register', { username, email, password });
+};
+
 export default api;
