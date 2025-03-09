9b8c14264b355ce855856aa6e89d15382ea77f8b
arnavsri454 <arnavsri454@gmail.com>
2025-03-09 14:22:41 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // ✅ Scan all React files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
 
