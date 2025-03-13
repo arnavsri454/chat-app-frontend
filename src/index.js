e459ad2275afc95b9d7afdb4d538f8863bd802c7
arnavsri454 <arnavsri454@gmail.com>
2025-03-13 15:09:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';  // ✅ Use HashRouter
+import { HashRouter } from 'react-router-dom';  // ✅ Import HashRouter
 import App from './App';
 import './index.css';
 
